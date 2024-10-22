c8691bf1a2008f14ea549743c29bf623de5c6cdf
teikv <levankiet1212.2004@gmail.com>
2024-10-22 18:42:20 +0700

    Update HW9 2

--- HW9/HW9.js
+++ HW9/HW9.js
@@ -1,12 +1,22 @@
 // example about set on JS 
 // Create a new Set
 const letters = new Set(["a","b","c"]);
+letters.add("d");
+letters.add("e");
+letters.add("f");
+// remove an element
+letters.delete("f");
+// Display set  
+console.log(letters);
 
 // Display typeof
 console.log( "The type of the set is " + typeof letters) ;
 
 // Create a new Map
 const numbers = new Map([[1,"one"],[2,"two"],[3,"three"]]);
-
+//add new elements
+numbers.set(4,"four");
+numbers.set(5,"five");
+numbers.set(6,"six");
 //Display map 
 console.log(numbers);
