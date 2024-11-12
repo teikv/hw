7d8082ed2e46df9099f87f02da0bad8114ac1fab
teikv <levankiet1212.2004@gmail.com>
2024-11-12 19:28:53 +0700

    update mysql

--- square/app.js
+++ square/app.js
@@ -1,29 +1,26 @@
-														
-require('dotenv').config();															
-const express = require('express');															
-const bodyParser = require('body-parser');															
-const mongoose = require('mongoose');															
-const squareRoutes = require('./routes/squareRoutes');															
-															
-const app = express();															
-const PORT = process.env.PORT || 3000;															
-															
-// Kết nối MongoDB															
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })															
-.then(() => console.log('MongoDB connected'))															
-.catch(err => console.log(err));															
-															
-// Sử dụng EJS làm view engine															
-app.set('view engine', 'ejs');															
-app.set('views', './views');															
-															
-// Middleware															
-app.use(bodyParser.urlencoded({ extended: true }));															
-															
-// Sử dụng routes															
-app.use('/', squareRoutes);															
-															
-// Chạy ứng dụng															
-app.listen(PORT, () => {															
-console.log(`Server is running on http://localhost:${PORT}`);															
-});															
+const express = require('express');
+const bodyParser = require('body-parser');
+const rectangleRoutes = require('./routes/rectangleRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sử dụng EJS làm view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Route to handle root URL
+app.get('/', (req, res) => {
+    res.render('index', { perimeter: null });
+});
+
+// Sử dụng routes
+app.use('/', rectangleRoutes);
+
+// Chạy ứng dụng
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
--- square/controllers/squareController.js
+++ square/controllers/squareController.js
@@ -1,20 +1,17 @@
-const Square = require('../models/square');										
-										
-// Controller để hiển thị form										
-exports.showForm = (req, res) => {										
-res.render('index', { perimeter: null, area: null });										
-};										
-										
-// Controller để tính chu vi và diện tích và lưu vào MongoDB										
-exports.calculateSquare = async (req, res) => {										
-const { sideLength } = req.body;										
-										
-const perimeter = 4 * sideLength;										
-const area = sideLength * sideLength;										
-										
-// Tạo mới hình vuông và lưu vào MongoDB										
-const square = new Square({ sideLength, perimeter, area });										
-await square.save();										
-										
-res.render('index', { perimeter, area });										
-};										
+const Square = require('../models/square');									
+									
+// Controller để hiển thị form									
+exports.showForm = (req, res) => {									
+res.render('index', { perimeter: null, area: null });									
+};									
+									
+// Controller để tính chu vi và diện tích									
+exports.calculateSquare = (req, res) => {									
+const { sideLength } = req.body;									
+									
+const square = new Square(Number(sideLength));									
+const perimeter = square.getPerimeter();									
+const area = square.getArea();									
+									
+res.render('index', { perimeter, area });									
+};									
