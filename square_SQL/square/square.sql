b5cb794f507ceb19a742cb6d39c098565ab4631b
teikv <levankiet1212.2004@gmail.com>
2024-11-12 19:25:31 +0700

    update mysql

---
+++ square_SQL/square/square.sql
@@ -0,0 +1,8 @@
+USE yourdbname;
+
+CREATE TABLE squares (						
+id INT AUTO_INCREMENT PRIMARY KEY,						
+sideLength FLOAT NOT NULL,						
+perimeter FLOAT NOT NULL,						
+area FLOAT NOT NULL						
+);						
